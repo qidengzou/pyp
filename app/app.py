94341af254ca6a708fdaf0867dd7f3792be4b2cb
qidengzou <qidengzou@192.168.0.106>
2026-01-17 03:01:59 +0800

    feat: 添加fastapi v3

--- app/app.py
+++ app/app.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
-
+ 
 app = FastAPI()
-
-
+ 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return {"Python": "on Vercel"}
